e9daa3ca7c456b9fa2638035c3c7196df1fa497e
midori yoridori <yoridrill@gmail.com>
2018-03-11 09:38:34 +0900

    キャンバスサイズ調整

--- js/index.js
+++ js/index.js
@@ -72,7 +72,6 @@ new Vue({
             canvas: self.$refs.pCanvas,
             antialias: true
         });
-        self.pRenderer.setPixelRatio(window.devicePixelRatio);
         self.pRenderer.setSize(width, height);
 
         self.pScene = new THREE.Scene();
